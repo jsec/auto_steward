c7bd154b8a972365b6c4f31455e8916f17fda306
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-07-16 23:26:06 -0400

    experiments

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from pywinauto import application
 
-def test_notepad():
+
+def test_notepad(number, penalty):
     app = application.Application()
     app.start("Notepad.exe")
-    app.UntitledNotepad.Edit.type_keys("pywinauto Works!", with_spaces=True)
+    app.UntitledNotepad.Edit.type_keys(f"/tp {number} {penalty}", with_spaces=True)
+    app.UntitledNotepad.Edit.type_keys("{ENTER}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test_notepad()
+    test_notepad(35, 10)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
